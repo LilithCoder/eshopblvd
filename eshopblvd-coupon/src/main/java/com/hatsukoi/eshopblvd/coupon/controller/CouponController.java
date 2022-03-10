f997cee01ea52cb01dc24f0c3bf1fc00068c7896
Weilin Gao <weilinga@usc.edu>
2022-03-11 02:05:01 +0800

    优惠券系统整合nacos配置中心

---
+++ eshopblvd-coupon/src/main/java/com/hatsukoi/eshopblvd/coupon/controller/CouponController.java
@@ -0,0 +1,15 @@
+package com.hatsukoi.eshopblvd.coupon.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gaoweilin
+ * @date 2022/03/11 Fri 1:05 AM
+ */
+@RefreshScope
+@RestController
+@RequestMapping("/coupon")
+public class CouponController {
+}
